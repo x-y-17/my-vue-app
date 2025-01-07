40944623672e21727bdbad567f51a3eda3d21972
x-y-17 <648525485@qq.com>
2025-01-07 15:04:12 +0800

    Update base URL for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/my-vue-app/",
+  base: "/x-y-17/",
   plugins: [vue()],
   css: {
     preprocessorOptions: {
